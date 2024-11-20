cfff08b2c45b5cf90b3cbac233deb1db136cfd5c
Hritesh <hsworkmail.1@gmail.com>
2024-11-20 15:50:20 +0530

    Initial_Commit

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./About";
+import Layout from "./Layout";
+
+export {Home, Header, Footer, About, Layout}
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { Home, About, Layout } from './components'
+
+// 1st way to specify which route is for which element
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     Component: Home
+//   }
+// ])
+
+// 2nd way to specify which route is for which element
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      {/* <Route path='/contact' element={<Contact/>}/> */}
+      {/* <Route path='/github' element={<Github/>}/> */}
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
